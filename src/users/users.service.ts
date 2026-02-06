f9dac119267cbdaeee0f3b0676bcbc5e9817dee1
Anushiya <anushiyacse43@gmail.com>
2026-02-06 14:06:42 +0530

    databse connected

---
+++ src/users/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from 'generated/prisma/browser';
+@Injectable()
+export class UsersService {
+  create(createUserDto: Prisma.UserCreateInput) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: Prisma.UserUpdateInput) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
